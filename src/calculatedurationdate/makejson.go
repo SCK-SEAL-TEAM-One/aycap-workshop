943d68013ae23117f65bec5d30e9f7e8fdad1b71
ploy <chonnikan.t@kkumail.com>
2018-06-22 15:02:47 +0700

    [Edited] เพิ่มฟังก์ชัน MakeJson และเพิ่ม api CalculationDurationBetweenTwoDate

--- src/calculatedurationdate/makejson.go
+++ src/calculatedurationdate/makejson.go
@@ -12,18 +12,18 @@ type Duration struct {
 }
 
 func MakeJson(startDay, startMonth, startYear, endDay, endMonth, endYear int) Duration {
-	//totalDay := calculeteTotalDay(startDay, startMonth, startYear, endDay, endMonth, endYear)
-	hours := TransformDaysToHour(15082)
-	//minutes := TransformDayToMinutes(hounrs)
-	seconds := TransformMinutesToSeconds(21718080)
+	totalDay := CalculeteTotalDay(startDay, startMonth, startYear, endDay, endMonth, endYear)
+	hours := TransformDaysToHour(totalDay)
+	minutes := TransformDayToMinutes(hours)
+	seconds := TransformMinutesToSeconds(minutes)
 	secondsResult := fmt.Sprintf("%s %s", AddComma(int64(seconds)), "Seconds")
 
 	return Duration{
-		//from:    TransformDayToFullDate(startDay, startMonth, startYear),
-		//	to:      TransformDayFullDate(endDay, endMonth, endYear),
-		//days:    fmt.Sprintf(" %s %s", AddComma(totalDay), "Days"),
+		From:    TransformDateToFullDate(startDay, startMonth, startYear),
+		To:      TransformDateToFullDate(endDay, endMonth, endYear),
+		Days:    fmt.Sprintf(" %s %s", AddComma(totalDay), "Days"),
 		Seconds: secondsResult,
-		//minutes: fmt.Sprintf(" %s %s", AddComma(minutes), "Minutes"),
-		Hours: fmt.Sprintf("%s %s", AddComma(hours), "Hours"),
+		Minutes: fmt.Sprintf(" %s %s", AddComma(minutes), "Minutes"),
+		Hours:   fmt.Sprintf("%s %s", AddComma(hours), "Hours"),
 	}
 }
