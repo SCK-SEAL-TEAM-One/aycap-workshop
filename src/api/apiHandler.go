77a776b563f54443538134fc4d659cc694bb2522
ploy <chonnikan.t@kkumail.com>
2018-06-22 11:07:56 +0700

    [Created]: robot file and api

---
+++ src/api/apiHandler.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+func ApiCalculateDate(responseWriter http.ResponseWriter, request *http.Request) {
+	queryString := request.URL.Query()
+	startDay, _ := strconv.Atoi(queryString.Get("start_day"))
+	startMonth, _ := strconv.Atoi(queryString.Get("start_month"))
+	startYear, _ := strconv.Atoi(queryString.Get("start_year"))
+	endDay, _ := strconv.Atoi(queryString.Get("end_day"))
+	endMonth, _ := strconv.Atoi(queryString.Get("end_month"))
+	endYear, _ := strconv.Atoi(queryString.Get("end_year"))
+
+	durationResponse, _ := json.Marshal(calculatedurationdate.MakeJson(startDay, startMonth, startYear, endDay, endMonth, endYear))
+	responseWriter.Write(durationResponse)
+}
